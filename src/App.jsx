c5b59f8c02e360028ad82a46f96b297de901f03c
Favian Perez <142631192+FavianP@users.noreply.github.com>
2025-05-08 17:52:51 +0000

    making navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { LoadingScreen } from "./components/sections/LoadingScreen";
 import "./index.css";
+import {Navbar} from "./components/Navbar";
 function App() {
-
+const [isLoaded, setIsLoaded] = useState(false);
 
   return (
-    <>
- <LoadingScreen/>
+    <>{!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>} 
+    {""} 
+    <div classname={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-1 `}>
+       <Navbar />
+    </div>
     </>
-  )
+  );
 }
 
 export default App
