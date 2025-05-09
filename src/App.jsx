04e00ed0ce005060b1dc08aeda2f539c8b990aca
Favian Perez <142631192+FavianP@users.noreply.github.com>
2025-05-09 06:22:54 +0000

    not complete MobileMenu

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,18 @@ import './App.css'
 import { LoadingScreen } from "./components/sections/LoadingScreen";
 import "./index.css";
 import {Navbar} from "./components/Navbar";
+import { MobileMenu } from './components/MobileMenu';
 function App() {
 const [isLoaded, setIsLoaded] = useState(false);
-
+const [menuOpen, setMenuOpen] = useState(false);
   return (
     <>{!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>} 
     {""} 
-    <div classname={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-1 `}>
-       <Navbar />
+    <div classname={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100 `}>
+       <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
     </div>
-    </>
+    </> 
   );
 }
 
