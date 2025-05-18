5224044d9bb187e3f03127825fe69d5148a213c3
Fpthepig <86858579+Fpthepig@users.noreply.github.com>
2025-05-18 04:44:40 +0000

    project commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,11 @@ import {Navbar} from "./components/Navbar";
 import { MobileMenu } from './components/MobileMenu';
 import { Home } from './components/sections/Home'; 
 import { About } from './components/sections/About';
+import { Project } from './components/sections/Projects';
 function App() {
 const [isLoaded, setIsLoaded] = useState(false);
 const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <>{!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>} 
     {""} 
@@ -19,6 +21,7 @@ const [menuOpen, setMenuOpen] = useState(false);
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <Home />
       <About />
+      <Project/>
     </div>
     </> 
   );
